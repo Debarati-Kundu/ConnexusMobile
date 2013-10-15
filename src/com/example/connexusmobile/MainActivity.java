e82991fcf18887e8b0d7f09dc482edc72fe09b82
Debarati Kundu <debarati@utexas.edu>
2013-10-15 18:19:48 -0500

    Another commit

--- src/com/example/connexusmobile/MainActivity.java
+++ src/com/example/connexusmobile/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.connexusmobile;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -16,7 +18,11 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return true;	
 	}
 
+	public void viewAllStreams(View view) {
+		Intent intent = new Intent(this, DisplayAllStreamsActivity.class);
+		startActivity(intent);
+	}
 }
