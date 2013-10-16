bcc954c2d3ffd81af1541593d2a130c523f11463
Debarati Kundu <debarati@utexas.edu>
2013-10-16 11:10:20 -0500

    Another big big commit of binaries

---
+++ src/com/example/connexusmobile/DisplayAllStreamsActivity.java
@@ -0,0 +1,75 @@
+package com.example.connexusmobile;
+
+import java.net.URL;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+public class DisplayAllStreamsActivity extends Activity {
+
+	public String AppURL = "http://apt-connexus.appspot.com/";
+	public String AppServlet = "AllStreamsServletAPI";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display_all_streams);
+		setupActionBar();
+		
+		/*
+		AsyncHttpClient client = new AsyncHttpClient();   
+        client.get(AppURL + AppServlet, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(String response) {
+                System.out.println(response);                   
+            }
+            @Override
+            public void onFailure(Throwable throwable, String s) {
+                super.onFailure(throwable, s);
+                System.out.println("onFailure");
+           }*/
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.display_all_streams, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
