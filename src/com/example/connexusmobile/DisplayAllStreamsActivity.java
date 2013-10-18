583620705cb860e34b484fc5f16d55385eef1693
Debarati Kundu <debarati@utexas.edu>
2013-10-18 02:56:16 -0500

    HTTP Req

--- src/com/example/connexusmobile/DisplayAllStreamsActivity.java
+++ src/com/example/connexusmobile/DisplayAllStreamsActivity.java
@@ -1,7 +1,16 @@
 package com.example.connexusmobile;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
@@ -9,16 +18,18 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 public class DisplayAllStreamsActivity extends Activity {
 
-	public String AppURL = "http://apt-connexus.appspot.com/";
-	public String AppServlet = "AllStreamsServletAPI";
-	public String AppTotalTarget = AppURL + AppServlet;
+	public static String AppURL = "http://apt-connexus.appspot.com/";
+	public static String AppServlet = "AllStreamsServletAPI";
+	public static String AppTotalTarget = AppURL + AppServlet;
 	
 	private GetSteamsFromServer mGetSteamsFromServer;
 	@Override
@@ -27,30 +38,42 @@ public class DisplayAllStreamsActivity extends Activity {
 		setContentView(R.layout.activity_display_all_streams);
 		setupActionBar();
 		
-		URL targetServer = null;
-		try {
-			targetServer = new URL("http://apt-connexus.appspot.com/AllStreamsServletAPI");
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 		mGetSteamsFromServer  = new GetSteamsFromServer();
 	    mGetSteamsFromServer.execute(AppTotalTarget);
 	    
-
 	}
 
 	public class GetSteamsFromServer extends AsyncTask<String, Integer, String> {
 		@Override
-		protected String doInBackground(String... AppTotalTarget) {
-	//		Log.d(AppURL, AppTotalTarget.toString());
+		protected String doInBackground(String... fromWebsite) {
+			Log.d(AppTotalTarget, fromWebsite.toString());
 			Gson gson = new Gson();
+			Selector sel = new Selector();
+			String tstJson = gson.toJson(sel);
+            makeHTTPPOSTRequest(AppTotalTarget, tstJson);
 			return null;
 		}
 	}
 	
+	public static void makeHTTPPOSTRequest(String apiUrl, String tstJson) {
+        try {
+            HttpClient c = new DefaultHttpClient();
+            HttpPost p = new HttpPost("http://apt-connexus.appspot.com/AllStreamsServletAPI");//(apiUrl);
+            StringEntity se = new StringEntity(tstJson);
+            se.setContentEncoding("UTF-8");
+            se.setContentType("application/json");
+            p.setEntity(se);            
+            HttpResponse r = c.execute(p);              
+            HttpEntity entity = r.getEntity();
+            final int statusCode = r.getStatusLine().getStatusCode();
+            Log.d(AppTotalTarget, "Error Code " + statusCode);
+        }
+        catch(IOException e) {
+            System.out.println(e);
+        }      
+    }   
 
+	
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	private void setupActionBar() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
